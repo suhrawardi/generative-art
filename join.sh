0ebc48a0bcd3ca1f51a3d3cf4c0b344de1d1863b
jarra <suhrawardi@gmail.com>
2021-02-21 11:20:21 +0100

    Ffmpeg struggles

--- join.sh
+++ join.sh
@@ -13,7 +13,11 @@ do
   k=$(printf "%05d" $i)
   echo "second/twee/movie${k}.mp4"
   # ffmpeg -i $SLICEDPNGS -r 1 -c:v libx264 -y "second/twee/movie${k}.mp4"
-  ffmpeg -framerate 25 -i $SLICEDPNGS -c:v libx264 -pix_fmt yuv420p -b:v 100M -y "second/twee/movie${k}.mp4"
+  ffmpeg -i $SLICEDPNGS -pix_fmt yuv420p -y out.mp4
+  # ffmpeg -safe 0 -i $SLICEDPNGS -c:v libx264 -vf "fps=25,format=yuv420p" "second/twee/movie${k}.mp4"
 done
 
 # ffmpeg -f concat -safe 0 -i <$(ls -r second/twee/movie*.mp4) -c copy output.mp4
+
+#fmpeg -r 1/5 -i img%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p out.mp4
+
